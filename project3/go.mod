061213a195c07404db82a0378fd8836d8e2a3495
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2025-01-28 23:55:49 +0530

    project 3 - grud api , mysql , backend

--- project3/go.mod
+++ project3/go.mod
@@ -3,7 +3,9 @@ module github.com/ShreyashPG/project3
 go 1.22.1
 
 require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- project3/main.go
+++ project3/main.go
@@ -6,14 +6,27 @@ import (
 	"net/http"
 
 	"github.com/ShreyashPG/project3/pkg/routes"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func main(){
+func main() {
 	r := mux.NewRouter()
+
 	routes.RegisterBookStoreRoutes(r)
+
 	http.Handle("/", r)
-	fmt.Println("Server is running on port 9010");
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+	headers := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+	origins := handlers.AllowedOrigins([]string{"http://127.0.0.1:5500"})
+
+	fmt.Println("Starting server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(headers, methods, origins)(r)))
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+		log.Fatal(err)
+	}
 }
